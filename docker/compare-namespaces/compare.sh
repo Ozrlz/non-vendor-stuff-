854f1de6612c9c4ed414ba12b95a2e2eedbecd97
ozrlz <oscar.cucei@gmail.com>
2018-12-21 23:24:21 -0600

    [REF] Add ORS in awk and delete tr ;D

--- docker/compare-namespaces/compare.sh
+++ docker/compare-namespaces/compare.sh
@@ -1,5 +1,6 @@
-
 #! /bin/bash
 while read PID1 PID2; do 
     diff -y <(ls -lisah /proc/$PID1/ns/ | awk -F "->" '{print $2}') <(ls -lisah /proc/$PID2/ns/ | awk -F "->" '{print $2}'); 
-done <<< $(for container in $(docker container ls | grep ${1:-nginx} | awk '{print $1}'); do docker inspect ${container} -f '{{.State.Pid}}'; done | tr '\n' ' ' )
+done <<< $(for container in $(docker container ls | grep ${1:-nginx} | awk 'BEGIN {ORS=" "};{print $1}'); do docker inspect ${container} -f '{{.State.Pid}}'; 
+done)
+

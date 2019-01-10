8957507e77d3bce979e697e4af8eff5172cac86f
Yahir Alejandro <yahiralejandros@gmail.com>
2019-01-10 05:00:24 -0600

    Added prompt questions and validations to iperf script

--- gists/gke/network/iperf/simple_iperf3.sh
+++ gists/gke/network/iperf/simple_iperf3.sh
@@ -2,12 +2,23 @@
 # This will create a client-server test that can be run using kubectl directly
 # It deletes everything afterwards so be careful!
 
-set -ex
+set -e
 
+if [ "$(which kubectl)" = "" ]; then
+    echo "Kubectl doesn't seem to be installed or added to your path."
+    exit 1
+fi
 
 kubectl run iperf-server --image=networkstatic/iperf3 --command -- iperf3 -s &&
 SERVER="$(kubectl get pods -o wide|egrep -o 'iperf.*Running'|awk {'print $1'}|xargs kubectl describe pod|grep IP|awk {'print $2'})"
 echo "Press ^C+ if hostname is not correct."
 kubectl run -i -t iperf-client --image=networkstatic/iperf3 --command -- iperf3 -c $SERVER.default.pod.cluster.local &&
 sleep 5
-kubectl delete deploy --all
+while true; do
+    read -p "Next, it will delete all the deployments, do want to continue?" yn
+    case $yn in
+        [Yy]* ) kubectl delete deploy --all; break;;
+        [Nn]* ) exit 0;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done

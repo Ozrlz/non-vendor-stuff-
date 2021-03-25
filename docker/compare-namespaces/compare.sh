8e9cc71c4ad5ddbec306c51b22afdf8c0c99d560
ozrlz <oscar.cucei@gmail.com>
2021-03-25 12:46:21 -0600

    [REF] use readarray and a cleaner approach

--- docker/compare-namespaces/compare.sh
+++ docker/compare-namespaces/compare.sh
@@ -1,6 +1,3 @@
 #! /bin/bash
-while read PID1 PID2; do 
-    diff -y <(ls -lisah /proc/$PID1/ns/ | awk -F "->" '{print $2}') <(ls -lisah /proc/$PID2/ns/ | awk -F "->" '{print $2}'); 
-done <<< $(for container in $(docker container ls | grep ${1:-nginx} | awk 'BEGIN {ORS=" "};{print $1}'); do docker inspect ${container} -f '{{.State.Pid}}'; 
-done)
-
+readarray -t PIDS <<< "$(for container in $(docker container ls | grep ${1:-nginx} | awk 'BEGIN {ORS=" "};{print $1}'); do docker inspect ${container} -f '{{.State.Pid}}'; done)"
+diff -y <(ls -lisah /proc/${PIDS[0]}/ns/ | awk -F "->" '{print $2}') <(ls -lisah /proc/${PIDS[1]}/ns/ | awk -F "->" '{print $2}')

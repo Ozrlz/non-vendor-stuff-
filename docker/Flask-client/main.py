282d7b48ec1871902d8fabccc6fa2c6ff2df77dd
ozrlz <oscar.cucei@gmail.com>
2021-04-02 19:30:55 -0400

    [IMP] Add Prometheus metrics support :DDD

--- docker/Flask-client/main.py
+++ docker/Flask-client/main.py
@@ -1,14 +1,17 @@
 from flask import jsonify, Flask, request
 from os import environ
 from socket import gethostname
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
 FLASK_PORT = environ.get('PORT_NUMBER', 5000)
 
 @app.route('/')
 def root():
-    headers_list = ['Flask hostname', gethostname()]
-    headers_list.append(request.headers.to_list())
+    headers_list = [['Flask hostname', gethostname()]]
+    for i in request.headers.items():
+        headers_list.append(i)
     print (headers_list)
     return jsonify( headers_list )
 
